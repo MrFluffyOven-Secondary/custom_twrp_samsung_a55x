5366dbb7b039c91dfc6a0ce7703a4a26017e2f4b
MrFluffyOven <thenoobdevs2024@gmail.com>
2024-05-29 12:58:03 +0100

    edit kernel in boardconfig and mk files

--- device.mk
+++ device.mk
@@ -1,40 +1,9 @@
 DEVICE_PATH := device/samsung/a55x
 
-# API levels
-PRODUCT_SHIPPING_API_LEVEL := 32
-
-# A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier \
-    checkpoint_gc \
-    otapreopt_script \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
-
 # fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
-
-# Health
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-
+    
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# Product characteristics
-PRODUCT_CHARACTERISTICS := phone
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
-
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 34
--- twrp_a55x.mk
+++ twrp_a55x.mk
@@ -1,6 +1,9 @@
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# emulated storage
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -15,8 +18,3 @@ PRODUCT_MODEL := SM-A556B
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a55xnaxx-user 14 UP1A.231005.007 A556BXXS2AXD3 release-keys"
-
-BUILD_FINGERPRINT := samsung/a55xnaxx/a55:14/UP1A.231005.007/A556BXXS2AXD3:user/release-keys
